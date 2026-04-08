412fa7e3181a325bc42f0dd2d61b8fadc0db7926
avierkin03 <avierkin03@gmail.com>
2026-04-08 19:43:59 +0300

    added room_list view and template

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Room, Category, Booking
 
-# Create your views here.
+# функція представлення списку всіх кімнат
+def room_list(request):
+    all_rooms = Room.objects.all()
+    context = {
+        "rooms": all_rooms
+    }
+    return render(request=request, template_name="booking/room_list.html", context=context)
