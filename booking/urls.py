aaa8a0ee45f79507e74d6ef438b79b1f8ebac7f2
avierkin03 <avierkin03@gmail.com>
2026-04-26 19:37:48 +0300

    added rooms filter

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from booking import views
 
 urlpatterns = [
-    path("rooms/", views.room_list, name="rooms"),
+    path("", views.room_list, name="rooms"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -4,7 +4,24 @@ from .models import Room, Category, Booking
 # функція представлення списку всіх кімнат
 def room_list(request):
     all_rooms = Room.objects.all()
+    categories = Category.objects.all()
+
+    category_id = request.GET.get("category")
+    capacity = request.GET.get("capacity")
+    price_max = request.GET.get("price_max")
+
+    if category_id:
+        all_rooms = all_rooms.filter(category = category_id)
+    if capacity:
+        all_rooms = all_rooms.filter(capacity__gte = int(capacity))
+    if price_max:
+        all_rooms = all_rooms.filter(price__lte = float(price_max))
+
     context = {
-        "rooms": all_rooms
+        "rooms": all_rooms,
+        "category": categories,
+        "selected_capacity": capacity,
+        "selected_price_max": price_max,
+        "selected_category": category_id
     }
     return render(request=request, template_name="booking/room_list.html", context=context)
