ab24ad5ea44528b939c5fc9a0ce387a666f6f9f6
avierkin03 <avierkin03@gmail.com>
2026-04-26 16:04:06 +0300

    modifications after another teacher

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/admin/login/'
+LOGIN_REDIRECT_URL = '/rooms/'
