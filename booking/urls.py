5cfb5f52ab8ae29653eee5b07b6c34ba8868cee4
Oleksandr Averkin <oleksandr.a@devpower.pro>
2026-05-17 19:36:49 +0300

    added authentication

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.room_list, name="rooms"),
     path("booking/<int:room_id>", views.book_room, name="booking"),
     path("your-bookings/", views.user_bookings, name="user_bookings"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
+    path("register/", views.user_register, name="register"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room, Category, Booking
 from datetime import datetime
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate, logout
 
 # функція представлення списку всіх кімнат
 def room_list(request):
@@ -72,3 +74,40 @@ def user_bookings(request):
         "bookings": Booking.objects.filter(user=request.user).order_by('-start_date')
     }
     return render(request=request, template_name="booking/my_bookings.html", context=context)
+
+
+# функція представлення логіну користувача
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("rooms")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, template_name="auth/login.html", context={"form": form})
+
+
+# функція представлення логауту користувача
+def user_logout(request):
+    logout(request)
+    return redirect("rooms")
+
+
+# функція представлення рестрації користувача
+def user_register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect("rooms")
+    else:
+        form = UserCreationForm()
+
+    return render(request, template_name="auth/register.html", context={"form": form})
